a24288f6d0c8856b4f488e80f47fff717dd62fe1
elavagnim <>
2024-10-04 20:14:52 -0500

    Publish auction events

--- src/AuctionService/Data/AuctionDbContext.cs
+++ src/AuctionService/Data/AuctionDbContext.cs
@@ -1,4 +1,4 @@
-﻿using AuctionService.Entities;
+﻿using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionService.Data;
@@ -10,4 +10,13 @@ public class AuctionDbContext : DbContext
     }
 
     public DbSet<Auction> Auctions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
 }
